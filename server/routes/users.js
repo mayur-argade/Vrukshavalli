876f1a31f917fc7b22d48b3ed7ca6858b7061a64
mayur-argade <argademayur2002@gmail.com>
2022-07-12 16:32:50 +0530

    server uploaded

--- server/routes/users.js
+++ server/routes/users.js
@@ -16,7 +16,7 @@ if(req.body.password){
     try{
         const user = await User.findByIdAndUpdate(req.params.id, {$set: req.body, 
         });
-        res.status(200).json("account has been updated")
+        res.status(200).json(user)
     }
     catch(err){
         return res.status(500).json(err);
